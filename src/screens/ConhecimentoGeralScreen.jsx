fa60b2c623341428ff9d9c501009563050ddf22c
LipeProg <progestudy@gmail.com>
2024-04-21 21:45:06 -0300

    quiz ja funcionando

--- src/screens/ConhecimentoGeralScreen.jsx
+++ src/screens/ConhecimentoGeralScreen.jsx
@@ -1,19 +1,69 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button} from 'react-native'
-
-
-const opcoes = () => {
-    
-}
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const ConhecimentoGeralScreen = () => {
-    return (
-        <View>
-            <Text>ola</Text>
-        </View>
-    )
-}
+  const [perguntas, setPerguntas] = useState([
+    {
+      pergunta: "Qual é a capital do Brasil?",
+      opcoes: ["Rio de Janeiro", "São Paulo", "Brasília", "Belo Horizonte"],
+      respostas: "Brasília"
+    },
+    {
+      pergunta: "Quem escreveu 'Dom Quixote'?",
+      opcoes: ["Miguel de Cervantes", "William Shakespeare", "Charles Dickens", "Fyodor Dostoevsky"],
+      respostas: "Miguel de Cervantes"
+    },
+    {
+      pergunta: "Qual é o maior planeta do sistema solar?",
+      opcoes: ["Júpiter", "Terra", "Marte", "Vênus"],
+      respostas : "Júpiter"
+    },
+    {
+      pergunta: "Quem pintou a 'Mona Lisa'?",
+      opcoes: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo"],
+      respostas: "Leonardo da Vinci"
+    },
+    {
+      pergunta: "Qual é a moeda oficial do Japão?",
+      opcoes: ["Dólar", "Libra", "Iene", "Euro"],
+      respostas: "Iene"
+    },
+  ]);
 
-export default ConhecimentoGeralScreen
+  const [indexPerguntas, setIndexPerguntas] = useState(0);
+  const [pontos, setPontos] = useState(0);
 
-const styles = StyleSheet.create({})
+  const proximaPergunta = (respostaSelecionada) => {
+    if (respostaSelecionada === perguntas[indexPerguntas].respostas) {
+      setPontos(pontos + 1);
+    }else{
+        alert('Resposta errada')
+    }
+
+    const proxima = indexPerguntas + 1;
+    if (proxima < perguntas.length) {
+      setIndexPerguntas(proxima);
+    }else{
+        alert('fim do quiz')
+    }
+  };
+
+  return (
+    <View>
+      <Text>{perguntas[indexPerguntas].pergunta}</Text>
+      {perguntas[indexPerguntas].opcoes.map((respostas, index)=> (
+        <Button
+        key={index}
+        title={respostas}
+        onPress={() => proximaPergunta(respostas)}
+        />
+
+      ))}
+      <Text>Pontuação: {pontos}</Text>
+    </View>
+  );
+};
+
+export default ConhecimentoGeralScreen;
+
+const styles = StyleSheet.create({});
